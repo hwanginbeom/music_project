68147fa7ef53b5cc734129251cc4bfcaf12980fb
hwanginbeom <rydn2002@gmail.com>
2020-11-26 10:45:26 +0900

    20.11.26 | django + templates

--- music_project/musicApp/views.py
+++ music_project/musicApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request) :
+    # context = {'ment' : '여기까지 잘되시나요?'}
+    return render(request, 'index.html')
+
+def elements(request) :
+    # context = {'ment' : '여기까지 잘되시나요?'}
+    return render(request, 'elements.html')
+
+def generic(request) :
+    # context = {'ment' : '여기까지 잘되시나요?'}
+    return render(request, 'generic.html')
