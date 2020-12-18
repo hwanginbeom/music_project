bc1e898c84d169e420cc84158914390b57ec9db0
Park SeJin <sjsj9368@gmail.com>
2020-12-18 10:47:35 +0900

    2020.12.18 | change images_url | modify href

--- music_project/music_prj/music_prj/settings.py
+++ music_project/music_prj/music_prj/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'music_project',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Park3098!!',
         'HOST': 'localhost',
         'PORT': '3306',
 
